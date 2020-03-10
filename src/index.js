7a7e1b90e269b625a26e3c5e6014366f510dd9b6
cjp34 <carlparm@gmail.com>
2020-03-10 15:45:12 -0400

    Basic Functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", pageSetup)
+
+function pageSetup() {
+    document.getElementById('create-task-form').addEventListener("submit", processFormSubmit)
+  
+}
+
+function processFormSubmit(event) {
+    event.preventDefault()
+    let newTaskDescription = document.getElementById("new-task-description").value
+    let newTask = createNewTask(newTaskDescription)
+    appendNewTask(newTask)
+    event.target.reset()
+}
+
+function createNewTask(description) {
+    let task = document.createElement('li')
+    task.innerText = description
+    return task
+
+}
+
+function appendNewTask(e)  {
+    let table = document.getElementById('tasks')
+    table.appendChild(e)
+}
